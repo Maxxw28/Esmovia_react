9e9c81fc5bbd4b5a08998190f9a87fa2ef73d95f
Maxxw28 <makswyzykowski@gmail.com>
2025-07-28 12:32:10 +0200

    added backend and simple schema

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+app.use(express.json());
+app.use(cors());
+// ----------- WORKING AREA -------------
+
+
+// Example route to test the backend
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'BoomBat hell yeah' });
+});
+
+
+// -------------- END OF WORKING AREA -----------
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
+// frontend/src/App.jsx
+import { useState, useEffect } from 'react';
+import axios from 'axios'; // Import Axios
 import './App.css';
 
 function App() {
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		// Use Axios to make a GET request to the backend API
+		axios
+			.get('/api/data')
+			.then((response) => {
+				setData(response.data.message);
+			})
+			.catch((error) => {
+				console.error('There was an error fetching the data!', error);
+			});
+	}, []); // The empty dependency array ensures this effect runs once when the component mounts
+
 	return (
-		<>
-			<div>initialize vite react app</div>
-		</>
+		<div className="App">
+			<header className="App-header">
+				{data ? <p>{data}</p> : <p>Loadin1g...</p>}
+			</header>
+		</div>
 	);
 }
 
