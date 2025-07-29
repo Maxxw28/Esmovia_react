ddc3bed667a81d6a4d7287f916185effbcb9001a
okolacki <129507504+okolacki@users.noreply.github.com>
2025-07-29 12:33:05 +0200

    updates updates updates

--- frontend/src/pages/earnings/Selection.jsx
+++ frontend/src/pages/earnings/Selection.jsx
@@ -1,59 +1,62 @@
 import React from 'react'
-import Clicker from '/public/images/clicker.png'
-import Miner from '/public/images/miner.png'
-import Video from '/public/images/video.png'
+import Clickerph from '/public/images/clicker.png'
+import Minerph from '/public/images/miner.png'
+import Videoph from '/public/images/video.png'
+import Miner from './Miner.jsx';
+import Clicker from './Clicker.jsx';
+import { Link } from 'react-router-dom';
 
 export default function Selection() {
   return (
-    <div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className='flex flex-row justify-center'>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ml-5 mr-5">
     <a href="#">
-        <img class="rounded-t-lg" src={Clicker} alt="" />
+        <img className="rounded-t-lg" src={Clickerph} alt="" />
     </a>
-    <div class="p-5">
+    <div className="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Clicker</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Clicker</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Earn BatCoins by playing a game of Clicker. Put the sweat in and earn some coins!</p>
-        <a href="#" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Earn BatCoins by playing a game of Clicker. Put the sweat in and earn some coins!</p>
+        <Link to="clicker" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Play Clicker
-            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </Link>
     </div>
 </div>
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ml-5 mr-5">
     <a href="#">
-        <img class="rounded-t-lg" src={Miner} alt="" />
+        <img className="rounded-t-lg" src={Minerph} alt="" />
     </a>
-    <div class="p-5">
+    <div className="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Miner</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Miner</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Mine your BatCoins. Zero effort, let your device do the work!</p>
-        <a href="#" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Mine your BatCoins. Zero effort, let your device do the work!</p>
+        <Link to="miner" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Start Mining
-            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </Link>
     </div>
 </div>
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ml-5 mr-5">
     <a href="#">
-        <img class="rounded-t-lg" src={Video} alt="" />
+        <img className="rounded-t-lg" src={Videoph} alt="" />
     </a>
-    <div class="p-5">
+    <div className="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Watch Video</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Watch Video</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Top up your account by simply watching a video. Only one per day. <strong>Videos watched today: </strong></p>
-        <a href="#" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Top up your account by watching a video.<br/> <strong>Videos watched today: </strong></p>
+        <a href="#" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-purple-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Watch Video
-            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
         </a>
