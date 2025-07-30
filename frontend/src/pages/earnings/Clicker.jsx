a7331d859ca964c324dfeac8abf67a89370a9e83
okolacki <129507504+okolacki@users.noreply.github.com>
2025-07-30 11:46:51 +0200

    clicker prawie gotowy tylko podpiac do bazy

--- frontend/src/pages/earnings/Clicker.jsx
+++ frontend/src/pages/earnings/Clicker.jsx
@@ -2,23 +2,61 @@ import React from 'react'
 import Batphoto from '/images/batclicker.png'
 import { useState} from "react"
 import ReactDOM from "react-dom/client"
+import Coin  from '/images/batcoin.png'
 
 
 export default function Clicker() {
   const[count, setCount] = useState(0);
+  const[batCoins, setBatCoins] = useState(0);
+  const[floatingText, setFloatingText] = useState(null);
 
-  const handleClick = () => {
-    setCount(prevCount => prevCount + 1)
-      if(count === 1000){
-      setCount(0);
+const handleClick = () => {
+  const reward = randomCoins();
+
+  setCount(prevCount => {
+    const newCount = prevCount + 1;
+
+    if (newCount === 1000) {
+      const totalReward = reward + 10;
+      setBatCoins(prev => prev + totalReward);
+
+      setFloatingText(`+${totalReward}`);
+      setTimeout(() => setFloatingText(null), 500);
+
+      return 0;
     }
-  };
+
+    setBatCoins(prev => prev + reward);
+
+    setFloatingText(`+${reward}`);
+    setTimeout(() => setFloatingText(null), 500);
+
+    return newCount;
+  });
+};
+
+  function randomCoins(){
+    const roll = Math.random()*100;
+
+    if (roll < 0.001) return 100;
+    if (roll < 0.5) return 5;
+    if (roll < 2.501) return 2;
+    if (roll < 12.501) return 1;
+    return 0;
+  }
+
 
 
   return (
     <>
     <div className='flex justify-center'>
       <img src={Batphoto} onClick={handleClick} alt="Bat" className='w-110 border-solid border-gray-200 border-4 rounded-3xl shadow-md active:scale-110 transition-transform duration-200 cursor-pointer hover:border-purple-300'/>
+      {floatingText && (
+        <div className='absolute text-3xl font-bold text-purple-900 animate-float'>
+          {floatingText}
+          <img src={Coin} alt="" className='w-10'/>
+        </div>
+      )}
     </div>
       <div className='w-70 h-60 p-5 border-solid border-gray-200 border-4 rounded-3xl shadow-md absolute top-50 left-10 hover:border-purple-300'>
         <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Percent chances:</h2>
@@ -43,6 +81,7 @@ export default function Clicker() {
       <div className='flex justify-center'>
       <h1 className='text-3xl mt-10 text-purple-900 font-bold'>clicks {count} out of 1000</h1>
       </div>
+      <h2 className='text-2xl mt-8 text-black-700 font-semibold text-right'>BatCoins: {batCoins}</h2>
     </>
   )
 }
